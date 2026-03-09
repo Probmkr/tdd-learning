abb130f8de6dfe1bc8fe86df622ff239a831538b
Probmkr <Thanatos9911@gmail.com>
2026-03-09 19:56:38 +0900

    feat: 副作用でレッドバー

---
+++ Money-002-明白な実装/main.go
@@ -0,0 +1,13 @@
+package money
+
+type Dollar struct {
+	amount int
+}
+
+func NewDollar(amount int) *Dollar {
+	return &Dollar{amount}
+}
+
+func (d *Dollar) Times(multiplier int) {
+	d.amount *= multiplier
+}
---
+++ Money-002-明白な実装/money_test.go
@@ -0,0 +1,15 @@
+package money
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMultiplication(t *testing.T) {
+	five := NewDollar(5)
+	five.Times(2)
+	assert.Equal(t, 10, five.amount)
+	five.Times(3)
+	assert.Equal(t, 15, five.amount)
+}
