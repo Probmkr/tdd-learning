d9c75e20406764384e3f84ccba7a7122bf6f089e
Probmkr <Thanatos9911@gmail.com>
2026-03-09 20:31:34 +0900

    feat: いい感じ

--- Money-003-三角測量/money_test.go
+++ Money-003-三角測量/money_test.go
@@ -8,10 +8,8 @@ import (
 
 func TestMultiplication(t *testing.T) {
 	five := NewDollar(5)
-	product := five.Times(2)
-	assert.Equal(t, 10, product.amount)
-	product = five.Times(3)
-	assert.Equal(t, 15, product.amount)
+	assert.True(t, five.Times(2).Equals(NewDollar(10)))
+	assert.True(t, five.Times(3).Equals(NewDollar(15)))
 }
 
 func TestEquality(t *testing.T) {
